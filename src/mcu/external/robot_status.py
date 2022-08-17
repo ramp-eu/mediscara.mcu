0b15a4ebfd0e247b36fdf2c901a1065c043a46b0
puskapeti <p.puska@lasram.hu>
2022-08-17 16:13:00 +0200

    updated robot status handling

--- src/mcu/external/robot_status.py
+++ src/mcu/external/robot_status.py
@@ -1,9 +1,54 @@
 
-from mcu.models.user_defined import Service
+import logging
+from mcu.models.user_defined import Command, Service
+from mcu.config import add_tcp_server
+from mcu.protocols import Message
 
 
 class RobotStatusService(Service):
     """Service class for processing robot status messages"""
 
-    def target(self, payload):
-        self.result = "OK"
+    ROBOT_POWER = 'robotPower'
+    ROBOT_RUNNING = 'robotRunning'
+    ROBOT_WAITING = 'robotWaiting'
+    ROBOT_ERROR = 'robotError'
+
+    def __init__(self) -> None:
+        self.__tcp = add_tcp_server('0.0.0.0', 65432)
+        self.__tcp.register_callbacks(received=self.__tcp_received)
+
+        initial_attributes = {
+            self.ROBOT_POWER: False,
+            self.ROBOT_RUNNING: False,
+            self.ROBOT_WAITING: False,
+            self.ROBOT_ERROR: False,
+        }
+
+        Command.update_attributes(initial_attributes)
+
+    def __tcp_received(self, message: bytes):
+        msg = Message.parse(message=message)
+
+        # process the status message
+        # status message syntax:
+        #   STATUS| TRUE  |  TRUE  |  TRUE  |  FALSE
+        #           ^^^^     ^^^^     ^^^^     ^^^^^
+        #          POWER   RUNNING   WAITING   ERROR
+
+        if msg.type == Message.TYPE.STATUS:
+            logging.info(msg)
+            try:
+                attributes = {
+                    self.ROBOT_POWER: msg.data[0] == "TRUE",
+                    self.ROBOT_RUNNING: msg.data[1] == "TRUE",
+                    self.ROBOT_WAITING: msg.data[2] == "TRUE",
+                    self.ROBOT_ERROR: msg.data[3] == "TRUE"
+                }
+
+                logging.info("Updating attributes to: %s", attributes)
+
+            except IndexError:
+                logging.warning("Invalid status message: %s", msg)
+                return
+
+            Command.update_attributes(attributes)
